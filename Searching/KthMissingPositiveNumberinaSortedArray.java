3af3c6d3ea988e3fe4cf2707423323854fb5d406
harsh <sinha.harshraj1101@gmail.com>
2024-12-19 22:40:29 +0530

    Kth Missing Positive Number in a Sorted Array

---
+++ Searching/KthMissingPositiveNumberinaSortedArray.java
@@ -0,0 +1,14 @@
+public class KthMissingPositiveNumberinaSortedArray {
+    public static int findKthPositive(int[] arr, int k) {
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] <= k) k++;
+            else break;
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 7, 11};
+        int k = 5;
+        System.out.println(findKthPositive(arr, k));
+    }
+}
