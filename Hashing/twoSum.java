c09036317f6a85634d859cb3df9db5f9cbeffc3d
harsh <sinha.harshraj1101@gmail.com>
2024-12-26 22:42:48 +0530

    Two Sum - Pair with Given Sum

---
+++ Hashing/twoSum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class twoSum{
+    static boolean twoSum(int arr[], int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < arr.length ; i++){
+            int val = target - arr[i];
+            if(map.containsKey(val)) return true;
+            map.put(arr[i], i);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 45, 6, 10, 8};
+        int target = 16;
+        System.out.println(twoSum(arr, target));
+    }
+}
