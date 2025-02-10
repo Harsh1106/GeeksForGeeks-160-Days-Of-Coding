bc75b7ea9b496119c33cbd47e94553e8e32e8973
harsh <sinha.harshraj1101@gmail.com>
2025-02-10 19:43:39 +0530

    Maximum path sum from any node

--- Tree/Maximumpathsumfromanynode.java
+++ Tree/Maximumpathsumfromanynode.java
@@ -16,3 +16,16 @@ class Solution {
         return maxSum;
     }
 }
+public class Maximumpathsumfromanynode {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(obj.findMaxSum(root));
+    }
+}
